9ac9776f1027d25f5ce8de135ad261a060046630
NeuralLine <neurallines>
2021-05-31 22:20:06 +0100

    vuex practice

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,26 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    counter: 0
   },
   mutations: {
+    increaseCounter(state, randomNumber) {
+      state.counter += randomNumber
+    },
+    decreaseCounter(state) {
+      state.counter--
+    }
   },
   actions: {
+    increaseCounter({ commit }) {
+      axios(
+        'https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new'
+      ).then(res => {
+        commit('increaseCounter', res.data)
+      })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
